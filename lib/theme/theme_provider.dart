f41fe5f73bca427a40d78bfd8e79b3fa84ef286f
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-04-13 17:09:14 +0530

    format

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,21 +1,20 @@
-
 import 'package:flutter/material.dart';
 import 'package:theme_toggle/theme/theme.dart';
 
-class ThemeProvider extends ChangeNotifier{
+class ThemeProvider extends ChangeNotifier {
   ThemeData _themeData = lightMode;
 
   ThemeData get themeData => _themeData;
 
-  set themeData(ThemeData themeData){
+  set themeData(ThemeData themeData) {
     _themeData = themeData;
     notifyListeners();
   }
 
-  void toggleTheme(){
-    if(_themeData == lightMode){
+  void toggleTheme() {
+    if (_themeData == lightMode) {
       themeData = darkMode;
-    } else{
+    } else {
       themeData = lightMode;
     }
   }
